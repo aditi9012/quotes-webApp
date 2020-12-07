3e28177bdf1f0c0bc0e1858a05e25f9ce6f8e349
aditi9012 <aditi.1721it1011@kiet.edu>
2020-12-07 13:28:52 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-tk89ph
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-tk89ph)
---
+++ index.js
@@ -0,0 +1,24 @@
+// Import stylesheets
+import "./style.css";
+
+// Write Javascript code!
+
+const quotesDiv = document.getElementById("quotes");
+fetch("https://api.kanye.rest")
+  .then(res => res.json())
+  .then(quote => {
+    quotesDiv.innerHTML += `<p>${quote.quote}</p>`;
+  });
+
+let catButton = document.getElementById("give-cat");
+catButton.addEventListener("click", evt => {
+  let catDiv = document.getElementById("cat-pic");
+  fetch("https://api.thecatapi.com/v1/images/search?")
+    .then(res => res.json())
+    .then(cats => {
+      cats.forEach(cat => {
+        catDiv.innerHTML = `<h3>Wishing you the very best day!!!</h3>
+      <img src="${cat.url}" alt="kitty"/>`;
+      });
+    });
+});
